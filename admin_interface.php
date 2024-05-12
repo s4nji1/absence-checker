c6012cf0e6f94961a2993f3732f71712570ea263
hamza ait bouhou <159446531+s4nji1@users.noreply.github.com>
2024-05-12 17:05:42 +0100

    fourth

--- admin_interface.php
+++ admin_interface.php
@@ -14,6 +14,7 @@
             <input type="number" id="correctNumber" name="correctNumber">
             <button type="submit">Submit</button>
         </form>
+        <a href="list.php"><button>show list</button></a>
     </div>
 
     <?php
